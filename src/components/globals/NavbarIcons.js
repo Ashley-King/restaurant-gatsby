df91c27e7c551882d6a8ce80f638ce3449637df3
Ashley <ashley@gmail.com>
2021-06-19 11:42:21 -0400

    beginning layout and navbar

---
+++ src/components/globals/NavbarIcons.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+
+class NavbarIcons extends Component {
+    render(){
+        return(
+            <div>
+                component NavbarIcons
+            </div>
+        )
+    }
+}
+
+export default NavbarIcons
--- src/components/layout.js
+++ src/components/layout.js
@@ -7,46 +7,34 @@
 
 import * as React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
-
-import Header from "./header"
-import "./layout.css"
-
+import {createGlobalStyle} from 'styled-components'
+import Navbar from '../components/globals/Navbar'
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
+ 
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
+        <GlobalStyle/>
+        <Navbar/>
         <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `2rem`,
-          }}
-        >
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
-      </div>
+        
+      
     </>
   )
 }
+
+const GlobalStyle = createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  body{
+    font-family: 'Open Sans', sans-serif;
+    color: #262626;
+    background: #fff;
+  }
+`
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
